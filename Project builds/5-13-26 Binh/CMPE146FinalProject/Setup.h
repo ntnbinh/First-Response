5ca170874d71f91af7e3c9e938d0dee2726f2ad2
ntnbinh <80180304+ntnbinh@users.noreply.github.com>
2026-05-13 21:51:26 -0700

    make a copy

---
+++ Project builds/5-13-26 Binh/CMPE146FinalProject/Setup.h
@@ -0,0 +1,14 @@
+#ifndef SETUP_H
+#define SETUP_H
+
+void setup1();
+void send_message(char* msg); //  display message function 
+void wait_state(); 
+void start_game(); 
+void game_state(); 
+void timer_start(int timer1);
+uint32_t set_pin(); 
+uint32_t clear_pin(); 
+
+
+#endif
